b5fbcd78593b2f2e95d6df8bb040504a1d0cca5d
sam <sam@freighttrust.com>
2021-01-13 07:22:04 -0800

    ci(mutations): vertigo test

--- truffle-config.js
+++ truffle-config.js
@@ -39,9 +39,18 @@ module.exports = {
     api_keys: {
         etherscan: etherscanApiKey,
     },
-    plugins: ["truffle-plugin-verify", "@chainsafe/truffle-plugin-abigen"
-],
+    plugins: ["truffle-plugin-verify", "@chainsafe/truffle-plugin-abigen"],
     networks: {
+        vertigo_test_network_1: {
+            host: "127.0.0.1",
+            port: 8545,
+            network_id: "*",
+        },
+        vertigo_test_network_2: {
+            host: "127.0.0.1",
+            port: 8546,
+            network_id: "*",
+        },
         geth: {
             host: "localhost",
             port: 8045,
