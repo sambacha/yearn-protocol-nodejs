2d17ecc5e3ac33b7ed9d9d6bb26bda571ef332f2
sam bacha <sam@freighttrust.com>
2021-01-25 17:36:44 -0800

    fix(mappings): better mapping port for openzeppelin

--- scripts/npm-wrapper.bash
+++ scripts/npm-wrapper.bash
@@ -7,7 +7,8 @@ wget https://raw.githubusercontent.com/sambacha/yearn-protocol-nodejs/master/tru
 wget https://raw.githubusercontent.com/sambacha/yearn-protocol-nodejs/master/package.json
 echo -ne "==> Installing..."
 yarn install
-mv node_modules/\@openzeppelin/ node_modules/\@openzeppelin
-echo -ne "==> Comping..."
+# mapping openzeppelinV2 Brownie import for Truffle
+# node_modules/\@openzeppelin/ node_modules/\@openzeppelin
+export @openzeppelinV2/=$(pwd)/node_modules/@openzeppelin/ contracts/*.sol
 ./node_modules/.bin/truffle compile || exit 1
 echo -ne "Finished Generating, outputs in  $PWD/build/ "
