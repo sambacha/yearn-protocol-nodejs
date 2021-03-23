56d9c9f578c2d3db8ce4c2b0ace2db7bb5b6abaf
sam <sam@freighttrust.com>
2021-03-23 16:31:02 -0700

    refactor(repo): sync

--- scripts/revert_trace.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-export SOLIDITY_REVERT_TRACE=true
-SOLIDITY_COVERAGE=true wrapper-ci.sh
-
--- scripts/wrapper-ci.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-export CI=''
-export SOLIDITY_COVERAGE=true
-
-set -o errexit
-yarn
-yarn add --dev https://github.com/sc-forks/solidity-coverage.git#$COMMIT_REF
-cat package.json
-npm run coverage
